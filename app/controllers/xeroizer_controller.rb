f2ca63699a1bc27b0ee2b81228d3a4c63acf5810
Mateusz Czerwiński <mateusz.czerwinski@netguru.pl>
2016-05-24 14:19:11 +0200

    Make a connection with Xero API and return all contacts sorted by a name

--- app/controllers/xeroizer_controller.rb
+++ app/controllers/xeroizer_controller.rb
@@ -1,2 +1,12 @@
 class XeroizerController < ApplicationController
+  expose(:client) { Xeroizer::PrivateApplication.new(
+      Rails.application.secrets.xero_consumer_key,
+      Rails.application.secrets.xero_consumer_secret,
+      Rails.root.join(Rails.application.secrets.cert_path),
+      rate_limit_sleep: 2,
+  )}
+
+  def index
+    p client.Contact.all(:order => 'Name')
+  end
 end
