db5df0ccaf0c4ee34b348efae5ed34651dc60806
Mateusz Czerwiński <mateusz.czerwinski@netguru.pl>
2016-05-25 15:34:45 +0200

    Add a simple example with Xeroizer gem in order to retrieve list of contacts

--- app/controllers/xero_gateway_controller.rb
+++ app/controllers/xero_gateway_controller.rb
@@ -1,2 +1,13 @@
 class XeroGatewayController < ApplicationController
+  expose(:client) { XeroGateway::PrivateApp.new(
+    Rails.application.secrets.xero_consumer_key,
+    Rails.application.secrets.xero_consumer_secret,
+    Rails.root.join(Rails.application.secrets.cert_path)
+  )}
+
+  def index
+    @response = client.get_contacts(:type => :all, :sort => :name, :direction => :desc)
+    @contacts = @response.contacts if @response.success?
+  end
+
 end
